5c4620fc26be4a2fd66576be7991dc269129e394
naysikeira23 <146839127+naysikeira23@users.noreply.github.com>
2025-06-04 09:31:37 -0300

    v

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Por que o livro de matemática se suicidou?',
+    'Porque tinha muitos problemas!'
+)
+
+criaCartao(
+    'Vegetariano',
+    'O que é um vegetariano que come carne?',
+    'Um ex-vegetariano!'
+)
+
+criaCartao(
+    'Café',
+    'O que é um café que nunca foi tomado?',
+    'Um café "sem filtro"!'
+)
+
+criaCartao(
+    'Rato',
+    'O que é um rato que é bom em matemática?',
+    'Um "rato" calculista!'
+)
